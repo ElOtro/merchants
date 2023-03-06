8c8e33e9324aada68ddc20e9bf5f7a6e0761aef4
ElOtro <dmitryvp.spb@gmail.com>
2023-03-06 17:21:47 +0300

    Add users by type (#2)
    * added users
    
    * added tests for RegistrationsController and SessionsController

---
+++ merchants-api/spec/requests/v1/dashboard_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe V1::DashboardController, type: :request do
+  
+  let (:user) { create_user }
+  let (:sign_in_url) { '/users/sign_in' }
+
+  describe "GET /index" do
+    context 'When get in dashboard' do
+      before do
+          sign_in_with_api(user)
+      end
+  
+      it 'returns 200' do
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
---
+++ merchants-api/spec/support/api_helpers.rb
@@ -0,0 +1,12 @@
+module ApiHelpers
+
+  def sign_in_with_api(user)
+    post '/users/sign_in', params: {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+  end
+
+end

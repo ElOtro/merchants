b7f6f809e5459ddfbd541dfa0a3318375bee71b8
ElOtro <dmitryvp.spb@gmail.com>
2023-03-07 01:03:25 +0100

    added swagger

--- merchants-api/app/controllers/users/sessions_controller.rb
+++ merchants-api/app/controllers/users/sessions_controller.rb
@@ -11,16 +11,6 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
-
-    log_out_failure
-  end
-
-  def log_out_success
-    render json: { message: 'You are logged out.' }, status: :ok
-  end
-
-  def log_out_failure
-    render json: { message: 'Hmm nothing happened.' }, status: :unauthorized
+    head :no_content
   end
 end

30f7b347d9eb347f525f5a4f62c7608258ab44f8
ElOtro <dmitryvp.spb@gmail.com>
2023-03-20 09:48:56 +0300

    added hhtp service and some tests (#10)

--- merchants-api/spec/factories/merchants.rb
+++ merchants-api/spec/factories/merchants.rb
@@ -2,5 +2,17 @@
 
 FactoryBot.define do
   factory :merchant do
+    name { Faker::Company.name }
+    description { Faker::Company.industry }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+
+    trait :active do
+      status { true }
+    end
+
+    trait :inactive do
+      status { false }
+    end
   end
 end
